9e4c2febfcf7ae4a87c3aa6fc1949052225e5026
Cleberson José Ciliato <cleberson.ciliato@gmail.com>
2021-07-28 12:39:41 -0400

    first commit

---
+++ src/Order.test.ts
@@ -0,0 +1,16 @@
+import Order from "./Order";
+
+test("Não deve criar um pedido com CFP inválido", function(){
+    const cpf = "111.111.111-11";
+    expect(() => new Order(cpf)).toThrow(new Error("Invalid CPF"));
+});
+
+test("Deve criar um pedido com 3 itens", function(){
+    const cpf = "778.278.412-36";
+    const order = new Order(cpf);
+    order.addItem("Guitarra", 1000, 2);
+    order.addItem("Amplificador", 5000, 1);
+    order.addItem("Cabo", 30, 3);
+    const total = order.getTotal();
+    expect(total).toBe(7090);
+});
